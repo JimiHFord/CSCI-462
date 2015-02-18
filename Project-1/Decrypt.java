063062a000874611abd858e17c6ff056b5e3632f
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-02-18 15:44:09 -0500

    silly mistake
    can’t share the output because they both have different error messages

--- Project-1/Decrypt.java
+++ Project-1/Decrypt.java
@@ -40,26 +40,36 @@ public class Decrypt {
 	
 	public static void main(String[] args) {
 		if(args.length != 3) {
-			SharedErrorOutput.usage();
+			usage();
 		}
 		KeyHelper helper = new KeyHelper();
 		if(!helper.isValidKey(args[0])) {
-			SharedErrorOutput.displayError("The <key> must only consist of 26 unique letters of the english alphabet.");
+			displayError("The <key> must only consist of 26 unique letters of the english alphabet.");
 		}
 		int[] key = helper.initKey(args[0]);
 		String cipherText = null;
 		try {
 			cipherText = new String(Files.readAllBytes(Paths.get(args[1])));
 		} catch (IOException e) {
-			SharedErrorOutput.displayError("The <ctfile> is either too large, or it was not found.");
+			displayError("The <ctfile> is either too large, or it was not found.");
 		} catch (InvalidPathException e) {
-			SharedErrorOutput.displayError("The <ctfile> is either too large, or it was not found.");
+			displayError("The <ctfile> is either too large, or it was not found.");
 		}
 		Decrypt decryptor = new Decrypt(key,cipherText);
 		String plainText = null;
 				plainText = decryptor.plainText();
 		System.out.println(plainText);
 		
+	}
+	
+	private static void usage() {
+		System.err.println("java Decrypt <key> <ctfile> <ptfile>");
+		System.exit(1);
+	}
+	
+	private static void displayError(String msg) {
+		System.err.println(msg);
+		usage();
 	}
 }
 
--- Project-1/Encrypt.java
+++ Project-1/Encrypt.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
@@ -50,23 +53,42 @@ public class Encrypt {
 	
 	public static void main(String[] args) {
 		if(args.length != 3) {
-			SharedErrorOutput.usage();
+			usage();
 		}
 		KeyHelper helper = new KeyHelper();
 		if(!helper.isValidKey(args[0])) {
-			SharedErrorOutput.displayError("The <key> must only consist of 26 unique letters of the english alphabet.");
+			displayError("The <key> must only consist of 26 unique letters of the english alphabet.");
 		}
 		int[] key = helper.initKey(args[0]);
 		String plainText = null;
 		try {
 			plainText = new String(Files.readAllBytes(Paths.get(args[1])));
-		} catch (IOException e) {
-			SharedErrorOutput.displayError("The <ctfile> is either too large, or it was not found.");
-		} catch (InvalidPathException e) {
-			SharedErrorOutput.displayError("The <ctfile> is either too large, or it was not found.");
+		} catch (IOException | InvalidPathException e) {
+			displayError("The <ctfile> is either too large, or it was not found.");
 		}
 		Encrypt encryptor = new Encrypt(key, plainText);
 		String cipherText = encryptor.cipherText();
+		
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(args[2]);
+			writer.print("The first line");
+			writer.println("The second line");
+			writer.close();
+		} catch (FileNotFoundException e) {
+			displayError("The <ctfile> is either too large, or it was not found.");
+		}
+		
 		System.out.println(cipherText);
+	}
+	
+	private static void usage() {
+		System.err.println("java Encrypt <key> <ptfile> <ctfile>");
+		System.exit(1);
+	}
+	
+	private static void displayError(String msg) {
+		System.err.println(msg);
+		usage();
 	}
 }
