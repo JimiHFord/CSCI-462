1c65cb26e314c0c0b25f024a3776fc5937c1bacb
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-02-25 17:48:15 -0500

    adding documentation

--- Project-1/Encrypt.java
+++ Project-1/Encrypt.java
@@ -14,27 +14,42 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 
 /**
+ * Class encrypt is responsible for encrypting a file using Letter-RC4. The
+ * key for this program is passed into the runtime command line arguments and
+ * should only consist of 26 unique english letters (lowercase or uppercase) in
+ * any permutation. You must encrypt the plaintext file with the same key you 
+ * intend to use in decrypting the ciphertext file.
  * 
  * @author  Jimi Ford
  * @version 2-18-15
  */
 public class Encrypt {
 
+	// private data members
 	private final int KEY_LENGTH = KeyHelper.KEY_LENGTH;
 	private String plainText;
 	private int[] key;
 	
-	// Error checking is done in main
-	// prevent construction
-	private Encrypt() {
-		
-	}
-	
+	/**
+	 * Construct an Encrypt object. It is assumed error checking is done
+	 * in {@link #main(String[])} hence the private constructor.
+	 * 
+	 * @param key must consist of only 26 unique numbers ranging from 0 to 25
+	 * 		  and should only be 26 indices long.
+	 * @param plainText the plaintext to encrypt
+	 */
 	private Encrypt(int[] key, String plainText) {
 		this.key = key;
 		this.plainText = plainText.toUpperCase();
 	}
 	
+	/**
+	 * Perform the encryption of the plaintext. Characters that don't fall
+	 * within A-Z or a-z are thrown away (or discarded). Only english letters
+	 * are encrypted. The encryption always generates uppercase ciphertext.
+	 * 
+	 * @return the ciphertext for the given plaintext
+	 */
 	public String cipherText() {
 		StringBuilder builder = new StringBuilder();
 		KeystreamGenerator gen = new KeystreamGenerator(key);
@@ -49,6 +64,13 @@ public class Encrypt {
 		return builder.toString();
 	}
 	
+	/**
+	 * The main method for this program. This method should be launched from
+	 * the command line.
+	 * 
+	 * @param args should consist of only the &lt;key&gt; at index 0, 
+	 * 			&lt;ptfile&gt; at index 1, and &lt;ctfile&gt; at index 2.
+	 */
 	public static void main(String[] args) {
 		if(args.length != 3) {
 			usage();
@@ -77,13 +99,18 @@ public class Encrypt {
 		}
 	}
 	
-	// Private helper methods
-	
+	/**
+	 * Print usage message for program and gracefully exit.
+	 */
 	private static void usage() {
-		System.err.println("java Encrypt <key> <ptfile> <ctfile>");
+		System.err.println("usage: java Encrypt <key> <ptfile> <ctfile>");
 		System.exit(1);
 	}
 
+	/**
+	 * Display an error message, then call the {@link #usage()} method.
+	 * @param msg the error message to display
+	 */
 	private static void displayError(String msg) {
 		System.err.println(msg);
 		usage();
--- Project-1/KeystreamGenerator.java
+++ Project-1/KeystreamGenerator.java
@@ -23,16 +23,11 @@ public class KeystreamGenerator {
 	private final int KEY_LENGTH = KeyHelper.KEY_LENGTH;
 	private final int[] key;
 	
-	// prevent default construction without a key
-	@SuppressWarnings("unused")
-	private KeystreamGenerator() {
-		key = null;
-	}
-	
 	/**
 	 * Construct a KeystreamGenerator with the given key in its initial state.
-	 * @param 	key The key size must be exactly 26 and contain every number from
-	 * 			0 to 25 inclusive. The key is assumed to be not null;
+	 * @param 	key The key size must be exactly 26 and contain every 
+	 * 			number from 0 to 25 inclusive. The key is assumed to be not 
+	 * 			null.
 	 */
 	public KeystreamGenerator(int[] key) {
 		this.key = key.clone();
