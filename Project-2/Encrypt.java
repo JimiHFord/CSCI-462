ca00ff331dc6c9484938bb1ca9a701bfd60deb48
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-03-10 15:13:54 -0400

    pretty sure it's working

--- Project-2/Encrypt.java
+++ Project-2/Encrypt.java
@@ -23,9 +23,13 @@ public class Encrypt {
 			usage();
 		}
 		ARK3 ark = new ARK3(key,nonce);
-		System.out.println(ark.keyStateString());
-		byte m = (byte)(GF28.multiply(0b01100101, 0b11010110, SBox.IRREDUCIBLE) & 0xff);
-//		byte m = (byte)(GF28.multiply(0b10000000, 0b10000000, SBox.IRREDUCIBLE) & 0xff);
+//		System.out.println(ark.keyStateString());
+		// x6+x5+x2+x0 * x7+x6+x4+x2+x1
+//		byte m = (byte)(GF28.multiply(0b01100101, 0b11010110, SBox.IRREDUCIBLE) & 0xff);
+		// x7 * x7
+		byte m = (byte)(GF28.multiply(0b10000000, 0b10000000, SBox.IRREDUCIBLE) & 0xff);
+		// x7+x6+x4 * x6+x5+x0
+//		byte m = (byte)(GF28.multiply(0b11010000, 0b01100001, SBox.IRREDUCIBLE) & 0xff);
 		System.out.println(Hex.toString(m));
 //		System.out.println(Hex.toString(1));
 	}
