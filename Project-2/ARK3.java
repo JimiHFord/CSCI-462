96b15442423d2783f34c76514ca0ce30e0848ff1
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-03-11 00:28:26 -0400

    getting there

--- Project-2/ARK3.java
+++ Project-2/ARK3.java
@@ -4,21 +4,26 @@ import edu.rit.util.Packing;
 
 public class ARK3 {
 
+	public static final int NUMBER_OF_ROUNDS = 27;
+	public static final int KEY_ROTATION = 29;
+	
 	private final byte[] key, nonce;
 	private long kH, kL;
+	private int round;
 	
 	public ARK3(byte[] key, byte[] nonce) {
 		this.key = key.clone();
 		this.nonce = nonce.clone();
 		kH = Packing.packLongBigEndian(key, 0);
 		kL = Packing.packLongBigEndian(key, 8);
+		round = NUMBER_OF_ROUNDS;
 	}
 	
 	public String keyStateString() {
 		return Hex.toString(kH) + Hex.toString(kL);
 	}
 	
-	public void rotr(int amt) {
+	private void rotr(int amt) {
 		if(amt % Long.SIZE == 0) return;
 		long amtL = Long.SIZE - amt;
 		long hiShiftedL = kH << amtL;
@@ -29,6 +34,36 @@ public class ARK3 {
 		kH = loShiftedL | hiShiftedR;
 	}
 	
+	private int round() {
+		if(++round > NUMBER_OF_ROUNDS) {
+			round = 1;
+		}
+		return round;
+	}
+	
+	public long round(long input) {
+		long subkey = subkey();
+		System.out.print(Hex.toString(subkey) + '\t');
+		input ^= subkey;
+		input = subAndMix(input);
+		return input;
+	}
+	
+	private long subAndMix(long input) {
+		return Mix.mix(
+				SBox.permute(
+						SBox.box(input)));
+	}
+	
+	public long subkey() {
+		rotr(KEY_ROTATION);
+		kH = subAndMix(kH);
+		int add =  (int) (kL & 0xff);
+		add = (GF28.add(add, round()) & 0xff);
+		kL = (kL & 0xffffffffffffff00L) | add;
+		return kH;
+	}
+	
 	public byte[] encrypt(byte[] input) {
 		return null;
 	}
@@ -36,5 +71,22 @@ public class ARK3 {
 	public long encrypt(long input) {
 		
 		return 0;
+	}
+	
+	public static void main(String[] args) {
+		ARK3 ark = new ARK3(
+				"00000000000000000000000000000000".getBytes(), 
+				"0000000000000000".getBytes());
+		long plaintext = 0;
+		long ciphertext = 0;
+//		long ciphertext = 0x8114510682e7a4bf;
+		long subkey = 0;
+		for(int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+//			subkey = ark.subkey();
+//			System.out.println(Hex.toString(subkey));
+			ciphertext = ark.round(ciphertext);
+			System.out.println(Hex.toString(ciphertext));
+		}
+		
 	}
 }
--- Project-2/Mix.java
+++ Project-2/Mix.java
@@ -1,16 +1,21 @@
+import edu.rit.util.Packing;
+
 
 public class Mix {
 
 	public static long mix(long input) {
-		
-		byte a, b;
-		
-		
-		return 0;
+		byte[] bytes = new byte[8];
+		Packing.unpackLongBigEndian(input, bytes, 0);
+		long mix_1, mix_2, mix_3, mix_4;
+		mix_1 = mix(bytes[0], bytes[1]) << (16 * 3);
+		mix_2 = mix(bytes[2], bytes[3]) << (16 * 2);
+		mix_3 = mix(bytes[4], bytes[5]) << (16 * 1);
+		mix_4 = mix(bytes[6], bytes[7]);
+		return mix_1 | mix_2 | mix_3 | mix_4;
 	}
 	
-	private static int mix(final int a, final int b) {
-		int c, d;
+	private static long mix(final byte a, final byte b) {
+		long c, d;
 		c = 0xff & GF28.add(
 				GF28.multiply(0b00000010, a, SBox.IRREDUCIBLE),b);
 		
