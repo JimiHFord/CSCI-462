0efd0005d8108fee1ea5d830cc0e7555e4ab71cd
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-03-21 15:46:14 -0400

    documentation
    DONE

--- Project-2/Decrypt.java
+++ Project-2/Decrypt.java
@@ -1,22 +1,27 @@
+//******************************************************************************
+//
+// File:    Decrypt.java
+// Package: ---
+// Unit:    Class Decrypt
+//
+//******************************************************************************
+
 import java.io.IOException;
 
 
-
-
-
- 
 public class Decrypt {
 
-	public static final int CT_INDEX = 0;
-	public static final int PT_INDEX = 1;
-	public static final int KEY_INDEX = 2;
-	public static final int NONCE_INDEX = 3;
+	private static final int CT_INDEX = 0;
+	private static final int PT_INDEX = 1;
+	private static final int KEY_INDEX = 2;
+	private static final int NONCE_INDEX = 3;
 	
 	public static void main(String[] args) {
 		byte[] key = null, nonce = null, plaintext = null, ciphertext = null;
 		try {
 			if(args.length != 4) {
-				throw new SanitizationException("Incorrect number of arguments");
+				throw new 
+				SanitizationException("Incorrect number of arguments");
 			}
 			key = Sanitize.sanitizeKey(args[KEY_INDEX], "<key>");
 			nonce = Sanitize.sanitizeNonce(args[NONCE_INDEX], "<iv>");
@@ -31,7 +36,6 @@ public class Decrypt {
 		ark.setKey(KeyHelper.combineARK3KeyAndNonce(key, nonce));
 		
 		for(int i = 0; i < plaintext.length; i++) {
-			// QUESTION: 0xff
 			plaintext[i] = (byte) (ark.decrypt(ciphertext[i] & 0xff) & 0xff);
 		}
 		
