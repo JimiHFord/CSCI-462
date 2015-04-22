b6484f8c5e4f110cce161a3552ecdd6fe076a62d
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-04-22 17:41:42 -0400

    finished attack!

--- Project-4/RsaDecrypt.java
+++ Project-4/RsaDecrypt.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 
 import edu.rit.util.AList;
 
@@ -16,7 +18,8 @@ import edu.rit.util.AList;
 /**
  * This class attacks RSA by taking at least 2 pairs of A 2048-bit RSA public 
  * modulus, an RSA public exponent, and an RSA ciphertext and computing the
- * plaintext from this information.
+ * plaintext from this information. The name of this attack is called a 
+ * shared factor attack.
  * 
  * @author Jimi Ford (jhf3617)
  * @version 4-22-2015
@@ -34,6 +37,8 @@ public class RsaDecrypt {
 			usage();
 		}
 		AList<RsaGroupInput> input = new AList<RsaGroupInput>();
+		AList<String> output = new AList<String>();
+		RSA rsa = new RSA();
 		int count = 0;
 		String publicModulus = null, publicExponent = null, ciphertext = null;
 		try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
@@ -61,7 +66,7 @@ public class RsaDecrypt {
 			e.printStackTrace();
 		}
 		if(count!=0) {
-			error("Each \"group\" must consist of 3 values, each on "
+			error("Each \"group\" must consist of 3 numeric values, each on "
 					+ "its own line."+'\n'+
 					'\t'+"1. public modulus" +'\n' +
 					'\t'+"2. public exponent" + '\n'+
@@ -69,12 +74,41 @@ public class RsaDecrypt {
 		}
 		for(int i = 0; i < input.size(); i++) {
 			RsaGroupInput x = input.get(i);
-			System.out.println(x.publicModulus);
-			System.out.println(x.publicExponent);
-			System.out.println(x.ciphertext);
+		}
+		RsaGroupInput alex = input.get(0);
+		RsaGroupInput blake = input.get(1);
+		final BigInteger gcd = alex.publicMod.gcd(blake.publicMod);
+		for(int i = 0; i < input.size(); i++) {
+			blake = input.get(i);
+			crack(blake, gcd, output);
+		}
+		for(int i = 0; i < output.size(); i++) {
+			System.out.println(output.get(i));
 		}
 	}
 	
+	private static void crack(RsaGroupInput blake, BigInteger gcd, 
+			AList<String> output) {
+		BigInteger blake_prime = blake.publicMod.divide(gcd);
+		BigInteger blake_private = RSA.private_key(blake.pubExp, 
+				blake_prime, gcd);
+		output.addLast(convertBigInt(
+				RSA.decrypt(blake.ciphertext, blake_private, 
+						blake.publicMod)
+				));
+	}
+	
+	private static String convertBigInt(BigInteger convert) {
+		byte[] bytes = convert.toByteArray();
+		int length = bytes[1] & 0xff;
+		byte[] s = new byte[length];
+		System.arraycopy(bytes, 2, s, 0, length );
+		return new String(s);
+	}
+	
+	/**
+	 * print usage message and exit
+	 */
 	private static void usage() {
 		System.err.println("usage: java RsaDecrypt <file>");
 		System.exit(1);
--- Project-4/RsaGroupInput.java
+++ Project-4/RsaGroupInput.java
@@ -1,7 +1,17 @@
+//******************************************************************************
+//
+// File:    RsaGroupInput.java
+// Package: ---
+// Unit:    Class RsaGroupInput
+//
+//******************************************************************************
+
 import java.math.BigInteger;
 
 /**
- * 
+ * This class acts as a simple data container for the different values
+ * in the input file. It exposes the BigInteger equivalent of the string
+ * values found in the input file.
  * 
  * @author Jimi Ford (jhf3617)
  * @version 4-22-2015
@@ -10,22 +20,28 @@ public class RsaGroupInput {
 	/**
 	 * A 2048-bit RSA public modulus.
 	 */
-	public final BigInteger publicModulus;
+	public final BigInteger publicMod;
 	
 	/**
 	 * An RSA public exponent.
 	 */
-	public final BigInteger publicExponent;
+	public final BigInteger pubExp;
 	
 	/**
 	 * An RSA ciphertext.
 	 */
 	public final BigInteger ciphertext;
 	
+	/**
+	 * construct an RsaGroupInput
+	 * @param publicModulus public modulus
+	 * @param publicExponent public exponent
+	 * @param ciphertext corresponding ciphertext
+	 */
 	public RsaGroupInput(String publicModulus,
 			String publicExponent, String ciphertext) {
-		this.publicModulus = new BigInteger(publicModulus);
-		this.publicExponent = new BigInteger(publicExponent);
+		this.publicMod = new BigInteger(publicModulus);
+		this.pubExp = new BigInteger(publicExponent);
 		this.ciphertext = new BigInteger(ciphertext);
 	}
 }
